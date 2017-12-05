4be16efce17d2fd62c9b59ff1f0a7a15f44559ba
Egor Madiarov <Georgeek@users.noreply.github.com>
2017-12-05 17:13:33 +0800

    4th commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,3 +4,17 @@ import $ from 'jquery';
 $(() => {
 	svg4everybody();
 });
+
+function initMap() {
+	const uluru = {lat: -25.363, lng: 131.044};
+	const map = new google.maps.Map(document.getElementById('google-map'), {
+		zoom: 4,
+		center: uluru
+	});
+	const marker = new google.maps.Marker({
+		position: uluru,
+		map: map
+	});
+}
+
+initMap();
